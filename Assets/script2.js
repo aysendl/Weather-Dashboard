d61adcf874a3ce8abf063f219bf376931c8a6c6c
Ayse <aysendl@gmail.com>
2020-05-10 15:08:12 -0700

    changes

--- Assets/script2.js
+++ Assets/script2.js
@@ -1,6 +1,7 @@
 console.log ("Start weather app");
 
-var cacheKey = "knownCities";
+var cacheKey = "known-cities";
+
 
 
 //Get mu starting data, try to load from local storage
@@ -9,6 +10,33 @@ var cities = JSON.parse(localStorage.getItem (cacheKey));
 if (!cities){
     cities =[]; 
 }
+
+showHistory();
+
+function showHistory() {
+  $("#citylist").html("");
+  for( var i=0; i<cities.length; i++){
+    var city = cities[i];
+
+    var cityEl = $("<li>");
+    cityEl.text(city);
+    cityEl.addClass("list-group-item");
+    cityEl.on("click",function(){
+      searchWeather($(this).text());
+    })
+    $("#citylist").append(cityEl);
+  }
+}
+
+// configuring search button
+var btn = $("#btn");
+btn.on('click', function(){
+  var searchCity = $("#city").val();
+  console.log(searchCity);
+  if(searchCity != "") {
+    searchWeather (searchCity);
+  } 
+});
 
 
 // Fetch the City
@@ -20,7 +48,6 @@ if (!cities){
 // Starting point
 function searchWeather (city){
   addNewCity(city);
-
   fetchWeather(city);
 }
 
@@ -28,7 +55,8 @@ function searchWeather (city){
 function fetchWeather (city){
   var queryParams = $.param({
     q:city,
-    appid:"ec0edd488270df0ee783102d43a65daf"
+    appid:"ec0edd488270df0ee783102d43a65daf",
+    units:"imperial"
   })
   console.log(queryParams)
   var queryUrl = "https://api.openweathermap.org/data/2.5/weather?"+queryParams
@@ -49,8 +77,21 @@ console.log(queryUrl)
 
 // Display the city data
 function displayWeather (cityData){
+  var date = moment().format('l');
+  var condition = cityData.weather[0].main; 
+  var cityName = cityData.name + " (" + date + ") " + condition;
+  $("#CityName").text(cityName);
 
+
+  var temp = "Temperature: " + cityData.main.temp +" F";
+  var hum = "Humidity: " + cityData.main.humidity + " %";
+  var wind = "Wind Speed: " + cityData.wind.speed + " MPH";
+
+  $("#Temp").text(temp);
+  $("#Hum").text(hum);
+  $("#Wind").text(wind);
 }
+
 
 // Get weather from the API
 function fetchUV (coord){
@@ -61,7 +102,7 @@ function fetchUV (coord){
   })
   console.log(queryParams)
   var queryUrl = "https://api.openweathermap.org/data/2.5/uvi?"+queryParams
-console.log(queryUrl)
+  console.log(queryUrl)
   $.ajax({ 
     url: queryUrl,
     method: "GET",
@@ -73,7 +114,9 @@ console.log(queryUrl)
 
 //Display the UV data
 function displayUVData(UVData){
-
+  console.log (UVData); 
+  var uv ="UV Index: " + UVData.value;
+  $("#UV").text(uv);
 }
 
 // Get weather from the API
@@ -86,7 +129,7 @@ function fetchForcast (coord){
   })
   console.log(queryParams)
   var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?"+queryParams
-console.log(queryUrl)
+  console.log(queryUrl)
   $.ajax({ 
     url: queryUrl,
     method: "GET",
@@ -98,16 +141,22 @@ console.log(queryUrl)
 
 //Display the forecast data
 function displayForcast (forcastData){
-
+  console.log (forcastData); 
+for( var i=0; i<forcastData.list.length;i++){
+ var data = forcastData.list[i];
+ if( data.dt_txt.indexOf("12:00:00") != -1){
+   
+ }
 }
+
+
 function addNewCity(city){
-if (cities.indexOf (city)== -1){
+  if (cities.indexOf (city)== -1){
 
-    console.log ("Add new city")
-    cities.push(city);
-    localStorage.setItem(cacheKey, JSON.stringify(cities));
+      console.log ("Add new city")
+      cities.push(city);
+      localStorage.setItem(cacheKey, JSON.stringify(cities));
 
+  }
+  showHistory();
 }
-
-}
-searchWeather ("Santa Rosa")
